994b9d34aec584b4c40310170b877d0a75a9e304
Liam Murphy <liampm32@gmail.com>
2023-09-03 11:08:51 +1000

    Document `InstructionSet` a little better

--- src/primary/set.rs
+++ src/primary/set.rs
@@ -15,8 +15,19 @@ fn repeat(value: u16) -> u64 {
 /// which I found via. hashbrown.
 #[inline]
 fn find_u16(word: u64, value: u16) -> u32 {
+    // First we make a value where each 16-bit chunk is zero if and only if that
+    // chunk in the original word equals `value`.
     let cmp = word ^ repeat(value);
-    (cmp.wrapping_sub(repeat(0x0001)) & !cmp & repeat(0x8000)).trailing_zeros() / 16
+    // Then we use this algorithm that sets the MSB of every chunk which equals 0.
+    // However, it isn't perfect - if a chunk does equal 0, that first bit where we
+    // subtract 1 from each chunk will overflow (intentionally), but that'll then
+    // result in an extra 1 getting subtracted from the next chunk up.
+    // If that chunk is equal to 1, it'll also overflow and get marked as a 0.
+    // However, this is fine for our purposes, since we only care about the first 0
+    // and it doesn't matter if the upper bits get trashed.
+    let mask = cmp.wrapping_sub(repeat(0x0001)) & !cmp & repeat(0x8000);
+    // Then find the index of the first word which has its MSB set.
+    mask.trailing_zeros() / 16
 }
 
 /// A set of queued instructions.
@@ -25,12 +36,19 @@ fn find_u16(word: u64, value: u16) -> u32 {
 /// That's not needed anyway, since this should only ever be a faster lookup for
 /// whether or not an instruction's in the queue.
 ///
-/// It also has some other limitations which work for the case of checking what
-/// instructions are in the queue:
-/// - There can only be at most 4 instructions that set one square, since each
-///   instruction must come from another instruction and in the worst-case
-///   scenario, all 4 squares around a square are set.
-/// - There can only be one instruction that sets a given position on the net.
+/// It doesn't actually consider the whole instruction, either: it only
+/// considers its net position and the square it sets on the first cuboid.
+/// However, this works fine for us, since all that means is that we might
+/// accidentally disallow mapping the same net position to two different
+/// mappings which happen to have the same square on the first cuboid.
+/// That's a good thing: ideally we'd actually not allow _any_ instructions that
+/// set the same net position, it just happens to be better for performance to
+/// let it happen for now and then fix it up later.
+///
+/// Another limitation is that this can only store at most 4 instructions that
+/// set the same square. That's the most that there should ever be, since each
+/// instruction must come from another instruction and in the worst-case
+/// scenario, all 4 squares around a square are set.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub(super) struct InstructionSet<const CUBOIDS: usize> {
     /// For each square on the first cuboid, the net positions of the up-to-4
